e195ba4bbe2a9be32252d5aa6a25e43174d88d18
quino <quino@10.159.2.23>
2015-10-09 13:12:22 +0100

    Empezando práctica del robot recolector

---
+++ src/practica_robot_ia/Practica_Robot_IA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_robot_ia;
+
+/**
+ *
+ * @author quino
+ */
+public class Practica_Robot_IA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
